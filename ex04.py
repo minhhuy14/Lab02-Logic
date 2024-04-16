00e7fab7a59e7e899ba50bdc3adba915c8532d45
21120177 <minhhuy14012003@gmail.com>
2024-04-16 22:18:27 +0700

    modify read data from file

--- ex04.py
+++ ex04.py
@@ -5,28 +5,44 @@ def read_data_from_file(file_name):
     file_path=os.path.join(script_dir,file_name)
     f= open(file_path, 'r')
         # Read the statement
-    statement = f.readline().strip()
-        
-        # Read the number of clauses
-    num_clauses = int(f.readline().strip())
-        
+    data=[]
+
+    data=f.read().splitlines()
+
+    print(data)
+    statement=data[0]
+    print('statement',statement)
+    alpha_statement=[]
+   
+    st_clause=statement.split()
+    st_clause=list(filter(lambda x:x!='OR',st_clause))
+    alpha_statement.append(st_clause)
+          
+    statement_clause=list(filter(lambda x: x!= 'OR',statement))
+
+    print(statement_clause)
+
+    num_clauses = int(data[1])
+    
+    list_clauses=data[2:]
+    print('list',list_clauses)
         # Read the clauses
-    clauses = []
-    for _ in range(num_clauses):
-            # Split the line into literals and remove extra spaces
-            clause = f.readline().strip().split()
-            # Filter out empty strings in case of multiple spaces
-            clause = [literal for literal in clause if literal]
-            clauses.append(clause)
+    kb_clauses = []
+    for cnf in list_clauses:
+        clause= cnf.split()
+        clause=list(filter(lambda x:x!='OR',clause))
+        kb_clauses.append(clause)
     f.close()
+    
     cnf_data={
-          'statement':statement,
-          'clauses':clauses
+          'alpha_statement':alpha_statement,
+          'kb_clauses':kb_clauses
     }
     return cnf_data
+
 print(os.getcwd())
 # Example usage:
 data = read_data_from_file("input.txt")
 # print("Statement:", statement)
 # print("CNF Clauses:", cnf_clauses)
-print(data['statement'])
+print(data)
