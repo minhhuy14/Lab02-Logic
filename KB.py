55383ab2288a2d8f847ce26b518c3d2d8d9a10e6
21120177 <minhhuy14012003@gmail.com>
2024-04-19 17:35:17 +0700

    fix logic bug and complete all

--- KB.py
+++ KB.py
@@ -6,18 +6,18 @@ class KnowledgeBase:
     def addClause(self, clause):
         if clause not in self.clauses and not self.isContainComplementaryPair(clause):
             self.clauses.append(clause)
-
+    def isEmptyClause(self,clause):
+        return len(clause)==0
     def setNegativeAtom(self,atom):
         if atom[0]=='-':
             return atom[1:]
         else:
             return '-'+atom
+        
     def setNegativeStatement(self, statement):
         negated_statement = []
-
         for clause in statement:
             negated_clause = []
-
             for atom in clause:
                 negated_atom = self.setNegativeAtom(atom)
                 negated_clause.append(negated_atom)
@@ -26,6 +26,7 @@ class KnowledgeBase:
 
         return negated_statement
     
+    #Check if exists complementary pair in clause
     def isContainComplementaryPair(self, clause):
         for atom in clause:
             if self.setNegativeAtom(atom) in clause:
@@ -36,6 +37,7 @@ class KnowledgeBase:
         clause=list(dict.fromkeys(clause))
         ordered_atoms=[]
 
+        #Sort atom by alphabetical ordering
         for atom in clause:
             if atom[0] == '-':
                 ordered_atoms.append((atom[1], -1))
@@ -54,9 +56,9 @@ class KnowledgeBase:
         return normalized_clause
     
     def resolve(self, clause_i, clause_j):
-        print('clause i,j')
-        print(clause_i)
-        print(clause_j)
+        # print('clause i,j')
+        # print(clause_i)
+        # print(clause_j)
         new_clause = []
         for atom in clause_i:
             neg_atom = self.setNegativeAtom(atom)
@@ -65,53 +67,54 @@ class KnowledgeBase:
                     clause_i = [clause_i]
                 if isinstance(clause_j, str):
                     clause_j = [clause_j]
-                temp_c_i = clause_i.copy()
-                temp_c_j = clause_j.copy()
-                print('temp')
-                print(temp_c_i)
-                print(temp_c_j)
-                temp_c_i.remove(atom)
-                temp_c_j.remove(neg_atom)
-                if not temp_c_i and not temp_c_j:
+                    
+                new_clause_i = clause_i.copy()
+                new_clause_j = clause_j.copy()
+
+                new_clause_i.remove(atom)
+                new_clause_j.remove(neg_atom)
+                if not new_clause_i and not new_clause_j:
                     new_clause.append(['{}'])
                 else:
-                    clause = temp_c_i + temp_c_j
+                    clause = new_clause_i + new_clause_j
                     clause = self.normalizeClause(clause)
-                    print('clause after normalize ',clause)
                     if not self.isContainComplementaryPair(clause) and clause not in self.clauses:
                         new_clause.append(clause)
-            print('current new clause: ',new_clause)
+            # print('current new clause: ',new_clause)
 
         return new_clause
     
     def PL_Resolution(self, stament):
-        tempKB = KnowledgeBase()
-        tempKB.clauses = self.clauses.copy()
+        newKB = KnowledgeBase()
+        newKB.clauses = self.clauses.copy()
 
-        neg_query = self.setNegativeStatement(stament)
-        print(neg_query)
-        for neg_atom in neg_query:
-            tempKB.addClause(neg_atom)
+        neg_stament = self.setNegativeStatement(stament)
         
-        print('tempKB ',tempKB.clauses)
+        for neg_atom in neg_stament:
+            newKB.addClause(neg_atom)
+        
         result = []
         while True:
-            clause_pairs = list(itertools.combinations(range(len(tempKB.clauses)), 2))
-            print(clause_pairs)
+            clause_pairs = list(itertools.combinations(range(len(newKB.clauses)), 2))
             resolvents = []
             for pair in clause_pairs:
-                resolvent = tempKB.resolve(tempKB.clauses[pair[0]], tempKB.clauses[pair[1]])
-                print('resolvent',resolvent)
-                if resolvent and resolvent not in resolvents:
+                resolvent = newKB.resolve(newKB.clauses[pair[0]], newKB.clauses[pair[1]])
+                
+                if ['{}'] in resolvent:
                     resolvents.append(resolvent)
-
+                    resolvents = list(itertools.chain.from_iterable(resolvents))
+                    result.append(resolvents)
+                    return result,True
+                
+                if resolvent and resolvent  not in newKB.clauses and resolvent not in resolvents:
+                    resolvents.append(resolvent)
+                
+    
+       
             resolvents = list(itertools.chain.from_iterable(resolvents))
             result.append(resolvents)
+            if resolvents==[] or resolvents is None:
+                return result,False
 
-            if not resolvents:
-                return result, False
-            if ['{}'] in resolvents:
-                return result, True
-            
             for res in resolvents:
-                tempKB.addClause(res)
+                newKB.addClause(res)
--- main.py
+++ main.py
@@ -1,55 +1,70 @@
 import os
 import KB
 
-def read_data_from_file(file_name):
+def readDataFromFile(filename):
     script_dir=os.path.dirname(os.path.abspath(__file__))
-    file_path=os.path.join(script_dir,file_name)
+    file_path=os.path.join(script_dir,filename)
     f= open(file_path, 'r')
-    
-    data=f.read().splitlines()
-    
-    alpha=data[0:1]
 
-    statement=[]
+    data=f.read().splitlines()
+
+    statement=[cnf.split() for cnf in data[0:1]]
+    alpha=[]
    
-    for cnf in alpha:
-        st_clause=cnf.split()
-        st_clause=list(filter(lambda x:x!='OR',st_clause))
-        statement.append(st_clause) 
+    for cnf in statement:
+        list_atoms=[]
+        for atom in cnf:
+            if atom!='OR':
+                list_atoms.append(atom)
+        alpha.append(list_atoms)
 
     num_clauses = int(data[1])
+
+    list_clauses=[cnf.split() for cnf in data[2:]]
     
-    list_clauses=data[2:]
-    
+    #Initialize kb object to save knowledge base clause
     kb=KB.KnowledgeBase()
 
     for cnf in list_clauses:
-        clause= cnf.split()
-        clause=list(filter(lambda x:x!='OR',clause))
+        clause=[]
+        for atom in cnf:
+            if atom!='OR':
+                clause.append(atom)
         kb.addClause(clause)
 
     f.close()
     
-    return kb,statement
+    return kb,alpha
 
-kb,alpha = read_data_from_file("input.txt")
-
-print(kb.clauses)
-result,check=kb.PL_Resolution(alpha)
-print(result)
-print(check)
-
-for loop_res in result:
-            print(len(loop_res))
-            for clause in loop_res:
-                string = ''
-                for c in clause:
-                    string += c
-                    if c != clause[-1]:
-                        string += ' OR '
-                print(string)
+def writeResultToFile(result, success, filename):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, filename)
+    f= open(filename, 'w')
+    for res in result:
+        f.write(str(len(res)) + '\n')
+        for clause in res:
+            clause_str = ''
+            for atom in clause:
+                clause_str += atom
+                if atom != clause[-1]:
+                    clause_str += ' OR '
+            f.writelines(clause_str + '\n')
+    if success:
+        f.writelines('YES')
+    else: 
+        f.writelines('NO')
+        
+    print('Save results into output.txt successfully!')
                 
-if check:
-           print('YES')
-else:
-            print('NO')
+if __name__ == '__main__':      
+
+    kb,alpha = readDataFromFile("input.txt")
+    print('KB Clauses: ',kb.clauses)
+    print('alpha statement: ',alpha)
+    result,success=kb.PL_Resolution(alpha)
+    print('Result: ',result)
+    print('Success: ',success)
+
+    writeResultToFile(result,success,"output.txt")
+
+
